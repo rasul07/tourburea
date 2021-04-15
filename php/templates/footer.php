ce25e90fb5e0a7545f89b1b650df267981daaaf0
rasul07 <75904183+rasul07@users.noreply.github.com>
2021-04-15 16:43:17 +0500

    Add files via upload

---
+++ php/templates/footer.php
@@ -0,0 +1,3 @@
+<footer>
+			<a href="http://www.internet-technologies.ru/" title="Сатостроение от А до Я">Сайтостроение от А до Я</a><br>Все права защищены
+</footer>
---
+++ php/templates/header.php
@@ -0,0 +1,15 @@
+<header>
+			<a href="/" title="" class="logo"><img src="images/logo.png" alt="Туристическое агентство"></a>
+			<div class="phone">+7 (123) 45-67-89</div>
+
+			<nav>
+				<ul id="top_menu">
+					<li><a href="index.html">О компании</a></li>
+					<li><a href="services.html">Наши услуги</a></li>
+					<li><a href="contacts.html">Контакты</a></li>
+				</ul>
+			</nav>
+
+			<div class="header_img"><img src="images/header_img.jpg" alt="Туристическое агентство">
+			</div>
+</header>
